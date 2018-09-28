c0fecf8f75a04e3c431d967a1ca269d5a0b6ad8f
mavzerburak0 <177773@ttu.ee>
2018-09-28 17:54:44 +0000

    pythonanywhere

--- m_blog/settings.py
+++ m_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p5&mnu9t^t*dkgu*xiwu!l(n%=qpxr*ko2t8(mnyv@$$1e*gim'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mavzer122.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
