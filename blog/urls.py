0234580a49c03d9d57d3ec39d3e1744654881602
mavzerburak0 <177773@ttu.ee>
2018-09-30 10:09:45 +0000

    design updates

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -8,5 +9,7 @@ urlpatterns = [
     path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
-    path('post/<int:pk>/remove/', views.post_remove, name='post_remove')
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 ]
