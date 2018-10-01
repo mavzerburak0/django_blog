0ac56c832a98d72de8a548b50effd9088c5f5fb4
mavzerburak0 <177773@ttu.ee>
2018-10-01 14:30:18 +0300

    heroku deploy error

--- m_blog/wsgi.py
+++ m_blog/wsgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "m_blog.settings")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'm_blog.settings')
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 
 from whitenoise.django import DjangoWhiteNoise
+
 application = DjangoWhiteNoise(application)
 
